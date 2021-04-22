e74b0809f843426806d19dd69a66b7674561ad53
Asmaa Elbanna <asmaaelbanna21@gmail.com>
2021-04-23 01:01:49 +0200

    TabsLayout

--- app/src/main/java/com/happycomp/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/happycomp/weatherforecast/MainActivity.kt
@@ -2,10 +2,35 @@ package com.happycomp.weatherforecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.happycomp.weatherforecast.fragments.*
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setUpdates()
+    }
+    fun setUpdates(){
+        val adapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment(),"Home")
+        adapter.addFragment(FavoriteFragment(),"Favorite")
+        adapter.addFragment(SettingsFragment(),"Settings")
+        adapter.addFragment(AlertFragment(),"Alert")
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+       // tabLayout.getTabAt(0)!!.setIcon()
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/happycomp/weatherforecast/fragments/PagerAdapter.kt
@@ -0,0 +1,51 @@
+package com.happycomp.weatherforecast.fragments
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+public class PagerAdapter(supportFragmentManager:FragmentManager) : FragmentPagerAdapter(supportFragmentManager,
+    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList =ArrayList<String>()
+
+
+
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList[position]
+//      when(position){
+//          0 -> {return HomeFragment()
+//          }
+//          1 -> {return FavoriteFragment()
+//          }
+//          2 -> {return SettingsFragment()
+//          }
+//          3 -> {return AlertFragment()
+//          }
+//          else -> {return HomeFragment()
+//          }
+//      } //end when
+
+    }
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mFragmentTitleList[position]
+    }
+
+    fun addFragment (fragment:Fragment,title:String){
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+
+
+
+
+
+
+
+
+}
