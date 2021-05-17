7ff05f9a545341e5e84a48cdc36e91995c173bdc
Asmaa Elbanna <asmaaelbanna21@gmail.com>
2021-05-17 17:14:50 +0200

    favorite screen

---
+++ app/src/main/java/com/happycomp/weatherforecast/FavoriteAdapter.kt
@@ -0,0 +1,61 @@
+package com.happycomp.weatherforecast
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.happycomp.weatherforecast.fragments.FavoriteFragment
+
+class FavoriteAdapter : RecyclerView.Adapter<FavoriteAdapter.MyHolder>(){
+
+    private val country = arrayOf("Egypt","Palestine","Iraq")
+    private val temp = arrayOf("20°","30°","40°")
+    private val status = arrayOf("snow","cloudy","rain")
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteAdapter.MyHolder {
+        val v = LayoutInflater.from(parent.context)
+                .inflate(R.layout.favorite_item, parent, false)
+        return MyHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteAdapter.MyHolder, position: Int) {
+
+        holder.countryText.text = country[position]
+        holder.tempText.text = temp[position]
+        holder.statusText.text = status[position]
+    }
+
+    override fun getItemCount(): Int {
+        return country.size
+    }
+
+    inner class MyHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+
+        var countryText: TextView
+        var tempText: TextView
+        var statusText: TextView
+
+        init{
+            countryText=itemView.findViewById(R.id.country_fav)
+            tempText=itemView.findViewById(R.id.temp_fav)
+            statusText=itemView.findViewById(R.id.status_fav)
+
+            itemView.setOnClickListener{
+                var position: Int =getAdapterPosition()
+                val context = itemView.context
+                val intent = Intent(context, FavoriteFragment::class.java).apply {
+                    putExtra("NUMBER", position)
+                    putExtra("CODE", countryText.text)
+                    putExtra("CATEGORY", tempText.text)
+                    putExtra("CONTENT", statusText.text)
+                }
+                context.startActivity(intent)
+            }
+        }
+
+    }
+
+}
--- app/src/main/java/com/happycomp/weatherforecast/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/happycomp/weatherforecast/fragments/FavoriteFragment.kt
@@ -5,23 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.happycomp.weatherforecast.FavoriteAdapter
 import com.happycomp.weatherforecast.R
+import kotlinx.android.synthetic.main.fragment_favorite.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoriteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavoriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<FavoriteAdapter.MyHolder>? = null
 
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -29,6 +29,8 @@ class FavoriteFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+
+     */
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,6 +40,14 @@ class FavoriteFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recycler_view_fav.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = FavoriteAdapter()
+        }
+
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
